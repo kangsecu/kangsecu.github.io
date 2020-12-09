854245c7efe0eebb731eeb1d494ae1ec1d1a56e0
kangsecu <kangsecu@gmail.com>
2020-12-10 02:34:09 +0900

    Update server.py

--- server.py
+++ server.py
@@ -20,41 +20,28 @@ client_socket,addr = server_socket.accept()
 
 #=====================connect=============================
 
-#key shared!
 key = [0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xaa,0xbb,0xcc,0xdd,0xee,0xff]
 
-key_state = block2state(key) 
-print("\n[*] key=")
-hex_print(key_state)
-
-#recv
+#Receive from client
 data = client_socket.recv(1024)
-print("\n[*] Received data =",data.decode())
+print("\n[*] Received data : ",data.decode())
 
-####### bytes -> string -> block -> state
-
-#byte to string
+#bytes -> string
 enc_string = data.decode()
-print("\n[*] enc_stirng = ")
-print(enc_string)
+print("\n[*] enc_string : ",enc_string)
 
-#string to block
+#string -> block
 enc_block = str2block(enc_string)
-print("\n[*] enc_block =")
-hex_block_print(enc_block)
+print("\n[*] enc_block : ",enc_block)
 
+#Decryption
+print("\n[*] Decryption now!")
+result_block = ECB_Dec(enc_block,key)
 
-#block to state
-enc_state = block2state(enc_block)
-print("\n[*] enc_state=")
-hex_print(enc_state)
-
-######### Decryption ####################
-print("\n[*] Decryption!!!")
-result_state= AES_Dec(enc_state ,key_state)
-
-print("\n[*] Recovered Plaintext =")
-hex_print(result_state)
+print("\n[*] Recovered Plaintext : ")
+for i in range(len(result_block)):
+	print(chr(result_block[i]), end=' ')
+print("")
 
 #====================disconnect============================
 # (E)
